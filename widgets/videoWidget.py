c16c151ce7c7df17d4fbad66f64618572e0248c3
odink <odinkohler@gmail.com>
2026-02-25 16:19:19 -0500

    videoWidget can now play video

--- widgets/videoWidget.py
+++ widgets/videoWidget.py
@@ -1,5 +1,7 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton
+from PySide6.QtMultimedia import QMediaPlayer
+from PySide6.QtMultimediaWidgets import QVideoWidget
+from PySide6.QtCore import QUrl
 
 
 class VideoWidget(QWidget):
@@ -10,8 +12,38 @@ class VideoWidget(QWidget):
     def _create_ui(self):
         layout = QVBoxLayout(self)
 
-        self.label = QLabel("Video Section")
-        self.label.setAlignment(Qt.AlignCenter)
-        self.label.setStyleSheet("background-color: black; color: white;")
+        self.video_widget = QVideoWidget()
+        layout.addWidget(self.video_widget)
 
-        layout.addWidget(self.label)
+        self.player = QMediaPlayer()
+        self.player.setVideoOutput(self.video_widget)
+
+        self.play_button = QPushButton("Play")
+        layout.addWidget(self.play_button)
+
+        self.play_button.clicked.connect(self.toggle_play_pause)
+        self.player.playbackStateChanged.connect(self._update_button)
+
+    def load_video(self, file_path):
+        self.player.setSource(QUrl.fromLocalFile(file_path))
+
+    def play(self):
+        self.player.play()
+
+    def pause(self):
+        self.player.pause()
+
+    def stop(self):
+        self.player.stop()
+
+    def toggle_play_pause(self):
+        if self.player.playbackState() == QMediaPlayer.PlaybackState.PlayingState:
+            self.player.pause()
+        else:
+            self.player.play()
+
+    def _update_button(self, state):
+        if state == QMediaPlayer.PlaybackState.PlayingState:
+            self.play_button.setText("Pause")
+        else:
+            self.play_button.setText("Play")
