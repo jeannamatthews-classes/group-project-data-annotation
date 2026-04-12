46fcbf7a9d17a23234c47720c9965a9b7a986430
Riley Emmons <rileygames484@icloud.com>
2026-04-12 01:30:46 -0400

    Fix x-axis values and add various improvements

--- widgets/graph_widget.py
+++ widgets/graph_widget.py
@@ -1,26 +1,18 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout
 import pyqtgraph as pg
 import numpy as np
-from datetime import datetime
 
 from util.hdf5_reader import HDF5Reader
 
-
 class TimeAxisItem(pg.AxisItem):
     def tickStrings(self, values, scale, spacing):
-        size = len(values)
-        hr = [0] * size
-        min = [0] * size
-        sec = [0] * size
-        mil = [0] * size
+        hr = [v / 1000 // 3600 for v in values]
+        min = [v / 1000 // 60 % 60 for v in values]
+        sec = [v // 1000 % 60 for v in values]
+        mil = [v % 1000 for v in values]
+        print(values)
 
-        for i, v in enumerate(values):
-            hr[i] = v / 1000 // 3600
-            min[i] = v / 1000 // 60 % 60
-            sec[i] = v // 1000 % 60
-            mil[i] = v % 1000
-
-        return ["%.2d:%.2d%.2d:%.4d" % (hr[i], min[i], sec[i], mil[i]) for i in range(len(values))]
+        return ["%.2d:%.2d:%.2d:%.3d" % (hr[i], min[i], sec[i], mil[i]) for i in range(len(values))]
 
 class Graph(QWidget):
     def __init__(self, reader: HDF5Reader):
@@ -28,42 +20,20 @@ class Graph(QWidget):
 
         self.reader = reader
 
+        self.current_time = 0
+        self.margin = 10000 # ms
+
         # single plot
         self._plot_init()
         self._create_ui()
 
-        if not self.reader:
-            return
-
-        # get sensors (assumes 2)
-        sensors = list(self.reader.get_sensors().keys())
-        if len(sensors) < 2:
-            raise ValueError("Need at least 2 sensors to overlay")
-
-        self.sensor_a = sensors[0]
-        self.sensor_b = sensors[1]
-
-        # two curves (different colors)
-        self.curve_a = self.plot.plot(pen="r", name=self.sensor_a)
-        self.curve_b = self.plot.plot(pen="b", name=self.sensor_b)
-
-        # optional legend
-        self.plot.addLegend()
-
-        # time state
-        self.current_time = 0
-        self.margin = 500 # ms
-
-        self.reader.set_min(self.sensor_b, 300)
-
-    def _plot_init(self):
-        axis = TimeAxisItem(orientation='bottom')
-        self.plot = pg.PlotWidget(axisItems={'bottom': axis})
-        self.plot.setMouseEnabled(x=False, y=False)
-
+        # leave empty plot if no data
         if self.reader is None:
             return
-        self.plot.setYRange(-self.reader.accl_range, self.reader.accl_range)
+
+        self._sensor_init()
+        self._curve_init()
+        self._find_offset()
 
     def _create_ui(self):
         layout = QVBoxLayout()
@@ -75,21 +45,66 @@ class Graph(QWidget):
 
         data_a, *_ = chunks.get(self.sensor_a)
         data_b, *_ = chunks.get(self.sensor_b)
+
         # --- SENSOR A ---
         if data_a is not None and len(data_a) > 0:
             y_a = data_a[:, 0]
-
-            # reconstruct index axis
-            x_a = np.arange(len(y_a))
+            x_a = data_a[:, 1] - self.offset_a
             self.curve_a.setData(x_a, y_a)
 
         # --- SENSOR B ---
         if data_b is not None and len(data_b) > 0:
             y_b = data_b[:, 0]
-
-            x_b = np.arange(len(y_b))
+            x_b = data_b[:, 1] - self.offset_b
             self.curve_b.setData(x_b, y_b)
+
+        self.plot.setXRange(self.current_time - self.margin, self.current_time + self.margin, 0)
+        self.vline.setPos(self.current_time)
 
     def update_position(self, position):
         self.current_time = position
         self.update_plot()
+
+    def _plot_init(self):
+        axis = TimeAxisItem(orientation="bottom")
+        self.plot = pg.PlotWidget(axisItems={"bottom": axis})
+        self.plot.setMouseEnabled(x=False, y=False)
+
+        if self.reader is None:
+            return
+
+        # multiply by 1.05 in order to only pad the top
+        self.plot.setYRange(0, self.reader.accl_range * 1.05, 0)
+        self.plot.addItem(pg.InfiniteLine(angle=0, pen=pg.mkPen("gray", width=1)))
+        self.vline = pg.InfiniteLine(pen=pg.mkPen("w", width=2))
+        self.plot.addItem(self.vline)
+
+    def _sensor_init(self):
+        # get sensors (assumes 2)
+        sensors = list(self.reader.get_sensors().keys())
+        if len(sensors) < 2:
+            raise ValueError("Need at least 2 sensors to overlay")
+
+        self.sensor_a = sensors[0]
+        self.sensor_b = sensors[1]
+
+    def _curve_init(self):
+        # two curves (different colors)
+        self.curve_a = self.plot.plot(pen="r", name=self.sensor_a)
+        self.curve_b = self.plot.plot(pen="b", name=self.sensor_b)
+
+        # optional legend
+        self.plot.addLegend()
+
+    def _find_offset(self):
+        # chunks.get() = [data, start, stop]
+        # data = [[val,time]]
+        # data[0][1] = first time
+
+        # temporary toggle to get only first item
+        self.reader.idx_mode = True
+        chunks = self.reader.get_chunks_by_time(0, 1)
+        self.reader.idx_mode = False
+
+        self.offset_a = chunks.get(self.sensor_a)[0][0][1]
+        self.offset_b = chunks.get(self.sensor_b)[0][0][1]
